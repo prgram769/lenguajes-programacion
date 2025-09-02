1c1a7005b10563768f1897521bf8c8cde2225a8d
prgram769 <prgram02@gmail.com>
2025-09-02 14:37:19 +0200

    proyecto reservadorCines acabado

--- C/proyectos/reservadorCines.c
+++ C/proyectos/reservadorCines.c
@@ -13,56 +13,147 @@ Requisitos:
 
 #include <stdio.h>
 
-void cargar(int mat[5][5]) {
-    int f, c, option;
+char charge(char chairs[5][5]) {
+    int f1, c1;
+    
+    printf("\nMensaje antes de cargar los asientos: Las O son asientos vacios y las X son asientos ocupados.\n\n");
 
-    printf("¿Quieres seleccionar un asiento?\n1. Si - 2. No.\n\n");
-
-    if (scanf("%d", &option) == 1)
+    for (int f = 0; f < 5; f++)
     {
-        printf("\nDime que asiento quieres elegir, me lo tienes que decir en este orden: fila y columna: \n\n");
-            
-        if (scanf("%d %d", &f, &c) == 2)
+        for (int c = 0; c < 5; c++)
         {
-            int option;
+            printf("[%c]", chairs[f][c]);
+        }
+        printf("\n"); 
+        
+    }
 
-            mat[f][c] = 1;
+    printf("\nDime la fila y la columna respectivamente del asiento que quieres: \n\n");
 
+    if (scanf("%d %d", &f1, &c1) == 2)
+    {
+        int option;
+        int confirmOption;
 
-                
+        printf("\n¿Estas seguro/a de que quieres ese asiento?\n1. Si. 2. No.\n\n");
+
+        if (scanf("%d", &confirmOption) == 1)
+        {
+            if (confirmOption == 1)
+            {
+                chairs[f1][c1] = 'X';
+            } else if (confirmOption == 2)
+            {
+                printf("\nSaliendo...\n\n");
+            } else {
+                printf("\nDebes introducir un valor contemplado.");
+            }   
         } else {
-            printf("\nDebes introducir valores validos.");
+            printf("\nDebes introducir valores validos.\n");
+        }
+
+        printf("\n¿Quieres añadir otro asiento?\n 1. Si. 2. No.\n\n");
+
+        if (scanf("%d", &option) == 1)
+        {
+            if (option == 1)
+            {
+                while (option == 1)
+                {
+                    printf("\nDime la fila y la columna respectivamente del asiento que quieres: \n\n");
+
+                    if (scanf("%d %d", &f1, &c1) == 2)
+                    {
+                        if (chairs[f1][c1] == 'X')
+                        {
+                            printf("\nEse asiento ya está ocupado, debes elegir otro");
+                        } else {
+                            int confirmOption;
+
+                            printf("\n¿Estas seguro/a de que quieres ese asiento?\n1. Si. 2. No.\n\n");
+
+                            if (scanf("%d", &confirmOption) == 1)
+                            {
+                                if (confirmOption == 1)
+                                {
+                                    chairs[f1][c1] = 'X';
+                                } else if (confirmOption == 2)
+                                {
+                                    printf("\nSaliendo...\n\n");
+                                } else {
+                                    printf("\nDebes introducir valores contemplados\n.");
+                                }
+                                
+                            } else {
+                                printf("\nDebes introducir valores validos.");
+                            }
+                            
+                        }
+                        
+                    } else {
+                        printf("\nDebes introducir valores validos.\n");
+                    }
+
+                    printf("\n¿Quieres añadir otro asiento?\n 1. Si. 2. No.\n\n");
+
+                    scanf("%d", &option);
+
+                    printf("\n");
+                }
+                
+            } else if (option == 2)
+            {
+                printf("\nSaliendo...\n\n");
+            } else {
+                printf("\nError, ese valor no esta contemplado, debes elegir uno que si lo esté.");
+            }         
+        } else {
+            printf("\nDebes introducir valores validos.\n");
         }
         
     } else {
-        printf("Introduce valores validos.\n");
+        printf("\nDebes introducir valores validos.\n\n");
     }
     
-}
-
-void imprimir(int mat[5][5]) {
-    int f, c;
-
-    for (f = 0; f < 5; f++)
+    for (int f = 0; f < 5; f++)
     {
-        for (c = 0; c < 5; c++)
+        for (int c = 0; c < 5; c++)
         {
-            printf("%d", mat[f][c]);
-            printf("\n");
+            printf("[%c]", chairs[f][c]);
         }
+        printf("\n"); 
         
     }
-    
-    
+
 }
 
 int main(void) {
-    int map[5][5];
+    int option;
+    char chairs[5][5] = {
+        {'O','O','O','O','O'},
+        {'O','O','O','O','O'},
+        {'O','O','O','O','O'},
+        {'O','O','O','O','O'},
+        {'O','O','O','O','O'}
+    };
+    
+    printf("--SISTEMA RESERVA ASIENTOS--\n\n");
+    printf("¿Quieres reservar un asiento?\n 1. Si. 2. No.\n\n");
 
-    printf("---SISTEMA DE RESERVA DE ASIENTOS---\n\n");
-    printf("-Mapa de asientos-\n[][][][][]\n[][][][][]\n[][][][][]\n[][][][][]\n[][][][][]\n\n");
-    cargar(map);
-    imprimir(map);
-
-     
+    if (scanf("%d", &option) == 1)
+    {
+        if (option == 1)
+        {
+            charge(chairs);
+        } else if(option == 2) {
+            printf("\nSaliendo...");
+            
+            return 0;
+        } else {
+            printf("\nError, debes introducir un valor valido dentro de las opciones contempladas.");
+        }
+    } else {
+        printf("\nIntroduce valores validos.");
+    }
+    
 }
