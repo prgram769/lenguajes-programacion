4629794070619f201d8c2dacc50d1f0074fd449a
prgram769 <prgram02@gmail.com>
2025-09-08 17:52:16 +0200

    nuevas cosas

---
+++ C/ejercicios/ej52.c
@@ -0,0 +1,9 @@
+/*1. Escriba una función que devuelva el resultado de elevar un número (declarado como
+entero) a cualquier potencia (valor entero). El resultado de este cálculo debe ser de
+tipo long.*/
+
+#include <stdio.h>
+
+int main(void) {
+    
+}
