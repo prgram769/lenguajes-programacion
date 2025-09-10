94d12430e11031795d5e84f605ce228e7ef32103
prgram769 <prgram02@gmail.com>
2025-09-10 13:25:00 +0200

    continuacion

---
+++ C/ejercicios/ej56.c
@@ -0,0 +1,52 @@
+/*5. La nota del examen de una asignatura corresponde en un 20% al primer parcial, un
+20% al segundo parcial y un 60% el examen final. La nota final es la mejor entre la
+nota del examen y la nota obtenida con la evaluación continua (teniendo en cuenta los
+dos parciales).
+Escriba un programa que pida estas tres notas y calcule la nota final de un alumno. A
+continuación, el programa debe preguntar al usuario si quiere calcular una nueva nota
+o finalizar (pulsando la tecla ‘s’). Utilice una función para calcular la nota y otra para
+obtener el máximo (que puede ser la desarrollada en el ejercicio 2).*/
+
+#include <stdio.h>
+
+float calcMark(float ex1, float ex2, float exFinal) {
+    float first = ex1 * 0.20;
+    float second = ex2 * 0.20;
+    float final = exFinal * 0.60;
+
+    float totalResult = first + second + final;
+
+    return totalResult;
+}
+
+float theBest(float totalResult, float exFinal) {
+    float best;
+
+    
+
+    if (totalResult)
+    {
+        
+    }
+    
+    return best;
+    
+}
+
+int main(void) {
+    float ex1, ex2, exFinal;
+    float mark;
+
+    printf("Debes introducir las notas de tus examenes en orden:\n\n");
+
+    if (scanf("%f %f %f", &ex1, &ex2, &exFinal) == 3)
+    {
+        float mean = (ex1 + ex2) / 2;
+        float finalMark = theBest(ex1, ex2, exFinal); 
+
+        printf("\nLa");
+    } else {
+        printf("\nDebes introducir valores validos.\n");
+    }
+    
+}
