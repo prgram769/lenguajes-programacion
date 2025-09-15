c157b5ee066a767cc7e62dc1d2868e3c9b8d7434
prgram769 <prgram02@gmail.com>
2025-09-15 16:45:57 +0200

    Principio de un ejercicio

---
+++ C/libro/ejercicios/ej1-9.c
@@ -0,0 +1,21 @@
+/*Escribe un programa que copie su entrada a la salida, reemplazando cada cadena de uno o mas blancos por un solo blanco*/
+
+#include <stdio.h>
+
+int main(void) {
+    int c;
+    int caracterAnterior;
+
+    while ((c = getchar()) != EOF)
+    {
+
+        if (c != ' ')
+        {
+            if (c != '\t')
+            {
+                
+            }
+        
+    }
+    
+}
