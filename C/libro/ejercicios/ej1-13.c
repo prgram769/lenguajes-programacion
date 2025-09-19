9a04eb9b71e252fb162fa1bc5a4fd3d6a8973ad6
prgram769 <prgram02@gmail.com>
2025-09-19 17:37:30 +0200

    planteamiento ejercicio 1-13 libro

---
+++ C/libro/ejercicios/ej1-13.c
@@ -0,0 +1,19 @@
+/*Escriba un programa que imprima el histograma de las longitudes de las palabras de su entrada. Es facil dibujar el 
+el histograma con las barras horizontales pero es un desafio con las verticales*/
+
+#include <stdio.h>
+
+int main(void) {
+    int wordsArray[20];
+    int counter;
+    
+    while ((counter = getchar()) != NULL)
+    {
+        if (counter != ' ' || counter != '\t' || counter != '\n')
+        {
+            
+        }
+        
+    }
+    
+}
