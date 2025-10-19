2278f0956231a12700bea7aadf60d192541cdb62
prgram769 <prgram02@gmail.com>
2025-10-19 13:52:15 +0200

    temario nuevo

--- C/temario/punteros.c
+++ C/temario/punteros.c
@@ -4,4 +4,34 @@ int main(void) {
     int edad = 37;
 
     printf("%p", &edad);
+
+    int n1 = 10;
+
+    int* pointer = &n1;
+
+    printf("\n%p", pointer);
+
+    // tipos de punteros
+
+    // NULL
+
+    int *NULLPointer = NULL;
+
+    printf("\n%p", NULLPointer);
+
+    // Void
+
+    void *voidPointer;
+
+    printf("\n%p", voidPointer);
+
+    voidPointer = &edad;
+
+    printf("\n%p", voidPointer);
+
+    // wild
+
+    int *wildPointer;
+
+    printf("\n%p", wildPointer);
 }
