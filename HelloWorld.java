728b2c3e2fa66d267c2500bc25d000e86b21dda4
prgram769 <prgram02@gmail.com>
2026-02-01 20:31:29 +0100

    avances java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+// This is a basic program that return hello world without class
+
+// public static void main(String[] args) {
+//   System.out.println("Hello world");
+// }
+
+// This is the same program but with class
+
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello world with class");
+  }
+}
