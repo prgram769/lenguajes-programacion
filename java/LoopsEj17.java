5527ef400ba645761bbddb4e5e0e4a359fd8243a
prgram769 <prgram02@gmail.com>
2026-02-05 20:24:47 +0100

    nuevos ejs de java

--- java/LoopsEj17.java
+++ java/LoopsEj17.java
@@ -4,7 +4,7 @@ es correcto (que es un número positivo).*/
 
 public class LoopsEj17 {
   public static void main(String[] args) {
-    System.out.println("Tell me one number");
+    System.out.print("Tell me one number ");
 
     int staticNumber = Integer.parseInt(System.console().readLine());
 
@@ -14,10 +14,29 @@ public class LoopsEj17 {
       return;
     }
 
-    int number = staticNumber;
+    int sum = 0;
 
-    for (int i = staticNumber; i <= staticNumber + 100; i++) {
-      System.out.println(number += i);
+    System.out.println("You want the sum in each iteration or a single at the end? 1.Sum in each iteration. 2.A single at the end.");
+
+    int option = Integer.parseInt(System.console().readLine());
+
+    if (option == 1) {
+      for (int i = staticNumber + 1; i <= staticNumber + 100; i++) {
+        sum += i;
+
+        System.out.println(sum);
+      }
+
+    } else if (option == 2) {
+      for (int i = staticNumber + 1; i <= staticNumber + 100; i++) {
+        sum += i;
+      }
+
+      System.out.println("The final sum is " + sum);
+
+    } else {
+      System.out.println("You musn't introduce other number other than 1 or 2");
     }
+
   }
 }
--- java/LoopsEj18.java
+++ java/LoopsEj18.java
@@ -3,4 +3,21 @@ dos números introducidos por teclado y validados como distintos, el programa
 debe empezar por el menor de los enteros introducidos e ir incrementando de
 7 en 7.*/
 
+public class LoopsEj18 {
+  public static void main(String[] args) {
+    System.out.println("Tell me 2 diferent numbers: ");
 
+    int number1 = Integer.parseInt(System.console().readLine());
+    int number2 = Integer.parseInt(System.console().readLine());
+
+    if (number1 == number2) {
+      System.out.println("The numbers must be diferent.");
+
+      return;
+    }
+
+    for (int i = 0; i <= 1; i++) {
+
+    }
+  }
+}
