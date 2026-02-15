4ed66b21f9e8eb751e11051551557e1a2c6d6701
prgram769 <prgram02@gmail.com>
2026-02-15 17:21:21 +0100

    ejercicio 30 java terminado

--- java/loops/LoopsEj30.java
+++ java/loops/LoopsEj30.java
@@ -12,6 +12,7 @@ Día: martes
 Hora: 20
 Entre las 18:00h del lunes y las 20:00h del martes hay 26 hora/s.*/
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
@@ -19,61 +20,103 @@ public class LoopsEj30 {
   public static void main(String[] args) {
     Scanner read = new Scanner(System.in);
 
-    System.out.print("Enter a week day, you can introduce the day like number or name: ");
-
-    String weekDay = read.nextLine();
-
-    String[] week = {"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"};
-
-    System.out.println(weekDay.charAt(0));
+    ArrayList<Integer> hoursDays = new ArrayList<>();
 
     int dayNumber = 0;
 
-    try {
-      dayNumber = Integer.parseInt(weekDay);
+    for (int i = 0; i < 2; i++) {
+      System.out.print("Enter a week day, you can introduce the day like number or name: ");
 
-      if (dayNumber < 1 || dayNumber > 7) {
-        System.err.println("That day doesn't exist.");
-
-        read.close();
-
-        return;
+      if (i == 1) {
+        read.nextLine();
       }
-    } catch (Exception e) {
-      if (!Arrays.asList(week).contains(weekDay.toLowerCase())) {
-        System.err.println("That day doesn't exist.");
 
-        read.close();
+      String weekDay = read.nextLine();
 
-        return;
-      } else {
-        switch (weekDay.toLowerCase()) {
-          case "monday":
-            dayNumber = 1;
-            break;
-          case "tuesday":
-            dayNumber = 2;
-            break;
-          case "wednesday":
-            dayNumber = 3;
-            break;
-          case "thursday":
-            dayNumber = 4;
-            break;
-          case "friday":
-            dayNumber = 5;
-            break;
-          case "saturday":
-            dayNumber = 6;
-            break;
-          case "sunday":
-            dayNumber = 7;
-            break;
+      String[] week = { "monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday" };
+
+      try {
+        dayNumber = Integer.parseInt(weekDay);
+
+        if (dayNumber < 1 || dayNumber > 7) {
+          System.err.println("That day doesn't exist.");
+
+          read.close();
+
+          return;
+        }
+      } catch (Exception e) {
+        if (!Arrays.asList(week).contains(weekDay.toLowerCase())) {
+          System.err.println("That day doesn't exist.");
+
+          read.close();
+
+          return;
+        } else {
+          switch (weekDay.toLowerCase()) {
+            case "monday":
+              dayNumber = 1;
+              break;
+            case "tuesday":
+              dayNumber = 2;
+              break;
+            case "wednesday":
+              dayNumber = 3;
+              break;
+            case "thursday":
+              dayNumber = 4;
+              break;
+            case "friday":
+              dayNumber = 5;
+              break;
+            case "saturday":
+              dayNumber = 6;
+              break;
+            case "sunday":
+              dayNumber = 7;
+              break;
+          }
         }
       }
-    }
-    
 
+      if (i == 1 && hoursDays.getFirst() > dayNumber) {
+        System.err.println("The second day must be higher than the first");
+
+        break;
+      }
+
+      hoursDays.add(dayNumber);
+
+      System.out.print("Enter a hour of the day in 24 hours format: ");
+
+      int hour = read.nextInt();
+
+      if (hour < 1 || hour > 24) {
+        System.err.println("That hour doesn't exist.");
+
+        break;
+      }
+
+      hoursDays.add(hour);
+    }
+
+    int day1 = hoursDays.getFirst();
+    int day2 = hoursDays.get(2);
+
+    int restDaysInHours = (day2 - day1) * 24;
+
+    int hourDay1 = hoursDays.get(1);
+    int hourDay2 = hoursDays.getLast();
+
+    int singleHours = hourDay1 - hourDay2;
+
+    if (singleHours < 0) {
+      singleHours *= -1;
+    }
+
+    int totalHours = restDaysInHours + singleHours;
+
+    System.out.printf("\nFrom the day %d at %d:00 hours to the %d day at %d:00 hours there are %d hours", day1, hourDay1, day2, hourDay2, totalHours);
 
     read.close();
   }
