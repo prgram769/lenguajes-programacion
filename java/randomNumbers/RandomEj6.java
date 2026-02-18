a580165eaaff945b74912e114df8b2833117bb46
prgram769 <prgram02@gmail.com>
2026-02-18 19:10:32 +0100

    nuevos ejercicios de java -> randomizando, ejs 6 y planteamiento del 7

--- java/randomNumbers/RandomEj6.java
+++ java/randomNumbers/RandomEj6.java
@@ -3,8 +3,39 @@ debe adivinarlo y tiene para ello 5 oportunidades. Después de cada intento
 fallido, el programa dirá cuántas oportunidades quedan y si el número intro-
 ducido es menor o mayor que el número secreto.*/
 
+import java.util.Scanner;
+
 public class RandomEj6 {
   public static void main(String[] args) {
+    Scanner read = new Scanner(System.in);
 
+    int randomNumber = (int)(Math.random() * 101);
+    int opportunities = 5;
+
+    while (opportunities != 0) {
+      System.out.print("Enter a number: ");
+
+      int userNumber = read.nextInt();
+
+      read.nextLine();
+
+      if (userNumber > randomNumber) {
+        opportunities--;
+
+        System.out.printf("Your number is higher than the secret number. You still have %d opportunities.\n", opportunities);
+      } else if (userNumber < randomNumber) {
+        opportunities--;
+
+        System.out.printf("Your number is lower than the secret number. You still have %d opportunities.\n", opportunities);
+      } else {
+        System.out.println("Congratulations!! That is the secret number.");
+
+        break;
+      }
+    }
+
+    System.out.printf("The secret number was %d", randomNumber);
+
+    read.close();
   }
 }
---
+++ java/randomNumbers/RandomEj7.java
@@ -0,0 +1,8 @@
+/*Escribe un programa que muestre tres apuestas de la quiniela en tres colum-
+nas para los 14 partidos y el pleno al quince (15 filas).*/
+
+public class RandomEj7 {
+  public static void main(String[] args) {
+
+  }
+}
