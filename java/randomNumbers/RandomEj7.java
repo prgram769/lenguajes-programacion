baf8027344684f7f8c67a5a109d8da6e51ef55cd
prgram769 <prgram02@gmail.com>
2026-02-19 20:54:22 +0100

    today base commit

--- java/randomNumbers/RandomEj7.java
+++ java/randomNumbers/RandomEj7.java
@@ -3,6 +3,10 @@ nas para los 14 partidos y el pleno al quince (15 filas).*/
 
 public class RandomEj7 {
   public static void main(String[] args) {
+    for (int i = 0; i < 15; i++) {
+      int number = (int)(Math.random() * 2 + 1);
 
+      System.out.println(number);
+    }
   }
 }
