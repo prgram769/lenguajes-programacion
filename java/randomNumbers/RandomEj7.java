c009ed2b04ccba8996636ec0e01aaaa4e53c5ff1
prgram769 <prgram02@gmail.com>
2026-02-20 23:49:59 +0100

    terminando ejercicio quiniela

--- java/randomNumbers/RandomEj7.java
+++ java/randomNumbers/RandomEj7.java
@@ -3,10 +3,46 @@ nas para los 14 partidos y el pleno al quince (15 filas).*/
 
 public class RandomEj7 {
   public static void main(String[] args) {
-    for (int i = 0; i < 15; i++) {
-      int number = (int)(Math.random() * 2 + 1);
+    int betCounter = 1;
+    int localCounter = 0;
+    int visitorCounter = 0;
 
-      System.out.println(number);
+    for (int i = 0; i < 3; i++) {
+      System.out.printf(" ".repeat(5) + "|--Bet %d--|", betCounter);
+
+      betCounter++;
+    }
+
+    System.out.println();
+
+    for (int i = 0; i < 14; i++) {
+      for (int j = 0; j < 3; j++) {
+        int randomNumberCol = (int) (Math.random() * 3);
+
+        if (randomNumberCol == 2) {
+          visitorCounter++;
+        } else if (randomNumberCol == 1) {
+          localCounter++;
+        }
+
+        if (randomNumberCol == 0) {
+          System.out.printf(" ".repeat(10) + "X" + "\t");
+
+        } else {
+          System.out.printf(" ".repeat(10) + randomNumberCol + "\t");
+        }
+      }
+
+      System.out.println();
+    }
+
+    int spaces = 3;
+
+    for (int i = 0; i < 3; i++) {
+      System.out.printf(" ".repeat(spaces) + "|-Full fifteen-|");
+
+
+      spaces--;
     }
   }
 }
