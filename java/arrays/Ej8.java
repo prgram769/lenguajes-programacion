947cd4ba3fd520d3cae6bbb1a1981eff36f1916a
prgram769 <prgram02@gmail.com>
2026-03-04 20:39:01 +0100

    empezando el ejercicio 8 de arrays, es hacer un grafico de barras horizontal con los datos de la temperatura media de cada mes en un año, estas seran pedidas al usuario por teclado

---
+++ java/arrays/Ej8.java
@@ -0,0 +1,24 @@
+/*Realiza un programa que pida la temperatura media que ha hecho en cada mes
+de un determinado año y que muestre a continuación un diagrama de barras
+horizontales con esos datos. Las barras del diagrama se pueden dibujar a base
+de asteriscos o cualquier otro carácter.*/
+
+import java.util.Scanner;
+
+public class Ej8 {
+  public static void main(String[] args) {
+    Scanner read = new Scanner(System.in);
+
+    double[] temp = new double[12];
+
+    for (int i = 1; i <= temp.length; i++) {
+      System.out.printf("Enter the average temperature of the %d month: ", i);
+
+      temp[i] = read.nextDouble();
+    }
+
+    // TODO hacer el grafico mediante un bucle interior y otro exterior de forma que pueda tener un grafico horizontal
+
+    read.close();
+  }
+}
