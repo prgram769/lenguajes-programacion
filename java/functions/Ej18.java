2767225ed4299eb0d8a95d290d144d61a6afd06a
prgram769 <prgram02@gmail.com>
2026-04-05 13:01:41 +0200

    ejercicios de funciones terminados

--- java/functions/Ej18.java
+++ java/functions/Ej18.java
@@ -1,28 +1,32 @@
 /*Escribe un programa que pase de decimal a binario.*/
 
 public class Ej18 {
+  public static int log2(int number) {
+    int result = (int) (Math.log(number) / Math.log(2));
+
+    return result;
+  }
+
   public static void main(String[] args) {
     int decimalNumber = Integer.parseInt(args[0]);
-    int counter = 0;
+    int binaryNumberLength = log2(decimalNumber) + 1;
 
-    while (decimalNumber != 0) {
-      decimalNumber /= 10;
+    int[] binaryDigits = new int[binaryNumberLength];
 
-      counter++;
+    for (int i = 0; i < binaryDigits.length; i++) {
+      binaryDigits[i] = decimalNumber % 2;
+
+      decimalNumber /= 2;
     }
 
-    decimalNumber = Integer.parseInt(args[0]);
+    long binaryFinalNumber = 0;
 
-    int binaryNumber = 1;
-
-    while (decimalNumber != 1) {
-      binaryNumber = decimalNumber % 2;
-
-      decimalNumber %= 2;
-
-      binaryNumber *= 10;
+    for (int j = binaryDigits.length - 1; j >= 0; j--) {
+      binaryFinalNumber *= 10;
+    
+      binaryFinalNumber += binaryDigits[j];
     }
 
-    System.out.println(binaryNumber);
+    System.out.println(binaryFinalNumber);
   }
 }
